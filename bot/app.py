247ecb335870bb9c96d2950573a291f44f2ae3d1
winchestor2001 <sbexruz2001@gmail.com>
2025-02-22 14:13:02 +0500

    [Feat] update app

--- bot/app.py
+++ bot/app.py
@@ -1,10 +1,17 @@
 import asyncio
+import os
+import sys
+
 from loader import bot, dp
 from handlers import router
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
