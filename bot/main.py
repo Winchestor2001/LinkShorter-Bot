dcaae2fd5624047b64d085fa548f0ed2a492b228
winchestor2001 <sbexruz2001@gmail.com>
2025-03-17 17:43:14 +0500

    [Chore] add try except to main file

--- bot/main.py
+++ bot/main.py
@@ -8,9 +8,12 @@ app = FastAPI()
 @app.get("/{short_code}")
 async def redirect(short_code: str):
     """Редиректит на оригинальный URL"""
-    short_code = short_code.lower()  # Делаем ключ в нижнем регистре
-    context, link = await code_finder(short_code)
-    original_url = LINKS.get(context)
+    try:
+        short_code = short_code.lower()  # Делаем ключ в нижнем регистре
+        context, link = await code_finder(short_code)
+        original_url = LINKS.get(context)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
 
     if original_url:
         return RedirectResponse(url=original_url)
